bdd2e52d771fd61e7aee36f0618e5a7c1e1bc4ee
Praful-Rathore-222 <rathorepraful420@gmail.com>
2021-05-10 12:30:03 +0530

    final commit

--- test.py
+++ test.py
@@ -7,18 +7,18 @@ import pandas as pd
 url = 'https://www.jodidata.org/_resources/files/downloads/gas-data/jodi_gas_csv_beta.zip'
 
 try:
-    # Get the requests
+    # Get the requests for the url
     request = requests.get(url, stream=True)
     # extracting the file
     get_unzipfile = zipfile.ZipFile(io.BytesIO(request.content))
-    # Read the Data
+    # Read the Data of unziped file
     Data = pd.read_csv(get_unzipfile.open(get_unzipfile.namelist()[0]))
     Data.head()
     # Get the Json Data
     df = Data.to_json(orient = 'records', date_format='iso')
 
-    # path of the current script 
-    path = '/home/rails/test-task'
+    # Get the current working directory(path)  
+    path = os.getcwd()
 
     # Specify the file name / To see the Json data
     file = 'myfile.json'
